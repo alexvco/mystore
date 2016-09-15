9cbc820d1795ff894c63916efc6861aee250b3b8
alexv7 <alexvarjabedian7@gmail.com>
2016-09-15 01:55:53 -0400

    deleted contacts table because it should be called spree_contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812190527) do
+ActiveRecord::Schema.define(version: 20160915051812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
